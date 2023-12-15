5777e9e679f647421d0645b374338b305700971d
ubuntu <fabboynice@gmail.com>
2023-12-15 10:12:23 +0100

    feat: auto install

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install binutils
+sudo apt-get install grub-common
 sudo apt-get install xorriso
 sudo apt-get install mtools
+sudo apt-get install grub-pc-bin
+sudo apt-get install llvm
