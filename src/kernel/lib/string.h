b7ec4d1f2b0d86d1446102489777b0438eec7248
Fabrice Schaub <fabrice.schaub@swisscom.com>
2023-12-18 07:26:21 +0100

    feat: added strlen and strcmp functions (needed by format)

--- src/kernel/lib/string.h
+++ src/kernel/lib/string.h
@@ -1,2 +1,7 @@
 #pragma once
-char* format(char* buf, ...);
+#include <stdint.h>
+#include <stdbool.h>
+
+//TODO: char* format(char* buf, ...);
+uint64_t strlen(const char* str);
+bool strcmp(const char* str1, const char* str2);
