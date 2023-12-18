afbabf174ec4e5f366b5cd2676b9652657990f9d
Fabrice Schaub <fabrice.schaub@swisscom.com>
2023-12-18 11:24:10 +0100

    fix: missings deps

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 sudo apt update
 sudo apt upgrade
+sudo apt install build-essential
+sudo apt install gcc-i686-linux-gnu
 sudo apt install binutils
 sudo apt-get install grub-common
 sudo apt-get install xorriso
 sudo apt-get install mtools
 sudo apt-get install grub-pc-bin
-sudo apt-get install llvm
 sudo apt install nasm
 sudo apt install qemu-system
 sudo apt install bochs
