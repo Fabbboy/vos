d2fb79980eeac2ae01461295ba5770394f4a4c40
RoBaertschi <72601088+RoBaertschi@users.noreply.github.com>
2023-12-19 12:31:55 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install binutils
-sudo apt-get install grub-common
-sudo apt-get install xorriso
-sudo apt-get install mtools
-sudo apt-get install grub-pc-bin
-sudo apt-get install llvm
+sudo apt install grub-common
+sudo apt install xorriso
+sudo apt install mtools
+sudo apt install grub-pc-bin
+sudo apt install llvm
 sudo apt install nasm
 sudo apt install qemu-system
