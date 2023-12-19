1bb28eda0851ed5d425d2abc33038ae16ebff7f0
RoBaertschi <72601088+RoBaertschi@users.noreply.github.com>
2023-12-19 14:20:30 +0100

    Upsi dupsi

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install build-essential gcc-i686-linux-gnu binutils grub-common xorriso mtools grub-pc-bin nasm qemu-systembochs libsdl2-dev libcanberra-gtk-module libcanberra-gtk3-module -y
+sudo apt install build-essential gcc-i686-linux-gnu binutils grub-common xorriso mtools grub-pc-bin nasm qemu-system bochs libsdl2-dev libcanberra-gtk-module libcanberra-gtk3-module -y
